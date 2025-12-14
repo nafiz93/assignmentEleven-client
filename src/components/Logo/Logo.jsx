2d5b0a166250de4d1b37b630c96fbd16bc2b33d1
Nobel <nobelfix93@gmail.com>
2025-12-14 21:54:19 +0100

    added the navbar and the footer and set the theme

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/images/logo10.png'
+
+export default function Logo() {
+  return (
+    <div className='flex items-center'>
+       
+       <img className='w-20' src={logo} alt="" /> 
+        <h1 className='font-bold -ms-3'>Assetverse</h1>
+        
+        
+        </div>
+  )
+}
--- src/layout/Rootlayout.jsx
+++ src/layout/Rootlayout.jsx
@@ -5,10 +5,14 @@ import Header from "../pages/Shared/header/Header";
 
 export default function Rootlayout() {
   return (
-    <div>
-      <Header></Header>
-      <Outlet></Outlet>
-      <Footer></Footer>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      
+      <Footer />
     </div>
   );
 }
